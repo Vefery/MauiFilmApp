c9940b0ad41cf6fbd162e012a7106fe347f330a5
Vefery <54257165+Vefery@users.noreply.github.com>
2025-08-27 18:22:13 +0700

    Initial commit

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiTestApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiTestApp.Pages")]
