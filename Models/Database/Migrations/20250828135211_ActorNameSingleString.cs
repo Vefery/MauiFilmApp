71137d6f1861d9f13e02c599c1cf1419f4266977
Vefery <54257165+Vefery@users.noreply.github.com>
2025-08-28 20:52:42 +0700

    Changed actor name to a single stirng

---
+++ Models/Database/Migrations/20250828135211_ActorNameSingleString.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MauiTestApp.Models.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActorNameSingleString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Actors");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "Actors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
